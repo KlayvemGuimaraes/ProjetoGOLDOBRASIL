2f1d02aa4573678309aab8770a300704125a9b56
Klayvemguimaraes <105517014+KlayvemGuimaraes@users.noreply.github.com>
2022-10-27 19:17:47 -0300

    Add files via upload

---
+++ copa_do_mundo/assets/js/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWEWFR0IBk5svEXbqNZx-C-t8jCqKlfKM",
+  authDomain: "copa-2022-917cb.firebaseapp.com",
+  projectId: "copa-2022-917cb",
+  storageBucket: "copa-2022-917cb.appspot.com",
+  messagingSenderId: "221357315785",
+  appId: "1:221357315785:web:8c91831b61594a78b05f18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
---
+++ copa_do_mundo/readme.md
@@ -0,0 +1,19 @@
+# Projeto Copa do Mundo
+
+![preview](./github/copa2022.png)
+
+> COPA DO CATAR
+
+[Clique aqui para acessar]()
+
+
+## 🌍 Tecnologias
+
+- HTML
+- CSS
+- JAVASCRIPT
+- GITHUB
+
+## 📚 Contato
+
+klayvemguik@gmail.com
